e71496665a5ea0174ef489c1d321c8f4d7d66abd
ale <ale91.dallas@gmail.com>
2024-01-25 14:01:25 +0100

    informazioni component

--- src/Componenti/ComponentiProfilo/InformazioniComponent.jsx
+++ src/Componenti/ComponentiProfilo/InformazioniComponent.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../../Style/InformazioniComponent.css";
+
+const informazioni = `Laureato in Chimica con esperienza di ricerca sull'upcycling di materiali plastici. Attualmente, sto sviluppando competenze come full stack web developer con l'obiettivo di combinare la mia profonda curiosità per la tecnologia con la visione di un futuro sostenibile.
+Durante il mio percorso formativo, ho affinato le mie abilità in diverse tecnologie e linguaggi di programmazione, dimostrando versatilità e una spiccata propensione all'apprendimento continuo.Dinamico ed eclettico, non limito la mia crescita al solo ambito professionale. Nutro numerose passioni e interessi, come la produzione musicale e la modellazione per la stampa 3D.`;
 
 export default function InformazioniComponent() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>InformazioniComponent</div>
-  )
+    <div className="informazioni-component p-3">
+      <h3>Informazioni</h3>
+      {informazioni.length < 150 || isOpen ? (
+        <p>{informazioni}</p>
+      ) : (
+        <>
+          {" "}
+          <p>{informazioni.substring(0, 150)} </p>{" "}
+          <div className="vedi-altro">
+          <p>...</p>
+            <p className="vedi-altro-btn" onClick={() => setIsOpen(!isOpen)}>
+              ...vedi altro
+            </p>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
