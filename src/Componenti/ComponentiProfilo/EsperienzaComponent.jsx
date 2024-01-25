b77b2afd86d0066a01b43beefae5fa06d7c1367c
Gregorio Vecchio <gregoriovecchio@gmail.com>
2024-01-25 14:47:57 +0100

    Mi bagno mi tuffo mi giro e mi rilasso

--- src/Componenti/ComponentiProfilo/EsperienzaComponent.jsx
+++ src/Componenti/ComponentiProfilo/EsperienzaComponent.jsx
@@ -2,6 +2,105 @@ import React from 'react'
 
 export default function EsperienzaComponent() {
   return (
-    <div>EsperienzaComponent</div>
+    <div className='border rounded px-4 pt-3 pb-0'>
+
+
+      <h4>Esperienza</h4>
+
+      <div className='esperienze mt-2 mb-0 pb-0'>
+
+        <div className='border-bottom mt-3' >
+        <div className="card mb-0 border border-0" style={{ maxWidth: 540 }}>
+          <div className="d-flex">
+            <div className="flex-shrink-0">
+              <img
+                src="https://www.mediakey.tv/fileadmin/news_import/pam_02.png"
+                className="img-fluid rounded-start"
+                alt="..."
+                style={{ width: 60, height: 60 }}
+              />
+            </div>
+            <div className="flex-grow-1">
+              <div className="card-body ps-2 pt-1">
+                <h5 className="card-title mb-1">Vice responsabile di punto vendita</h5>
+                <div className="card-text">
+                  <p className='mb-0 '>Pam PANORAMA S.p.A. · Part-time</p>
+                  <small className='mb-0 text-secondary'>ott 2021 - set 2022 · 1 anno</small>
+                </div>
+                <p className="text-body-secondary">
+                  Ravenna, Emilia Romagna, Italia
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        <div className='border-bottom mt-3' >
+        <div className="card mb-0 border border-0" style={{ maxWidth: 540 }}>
+          <div className="d-flex">
+            <div className="flex-shrink-0">
+              <img
+                src="https://www.mediakey.tv/fileadmin/news_import/pam_02.png"
+                className="img-fluid rounded-start"
+                alt="..."
+                style={{ width: 60, height: 60 }}
+              />
+            </div>
+            <div className="flex-grow-1">
+              <div className="card-body ps-2 pt-1">
+                <h5 className="card-title mb-1">Vice responsabile di punto vendita</h5>
+                <div className="card-text">
+                  <p className='mb-0 '>Pam PANORAMA S.p.A. · Part-time</p>
+                  <small className='mb-0 text-secondary'>ott 2021 - set 2022 · 1 anno</small>
+                </div>
+                <p className="text-body-secondary">
+                  Ravenna, Emilia Romagna, Italia
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        <div className='border-bottom mt-3' >
+        <div className="card mb-0 border border-0" style={{ maxWidth: 540 }}>
+          <div className="d-flex">
+            <div className="flex-shrink-0">
+              <img
+                src="https://www.mediakey.tv/fileadmin/news_import/pam_02.png"
+                className="img-fluid rounded-start"
+                alt="..."
+                style={{ width: 60, height: 60 }}
+              />
+            </div>
+            <div className="flex-grow-1">
+              <div className="card-body ps-2 pt-1">
+                <h5 className="card-title mb-1">Vice responsabile di punto vendita</h5>
+                <div className="card-text">
+                  <p className='mb-0 '>Pam PANORAMA S.p.A. · Part-time</p>
+                  <small className='mb-0 text-secondary'>ott 2021 - set 2022 · 1 anno</small>
+                </div>
+                <p className="text-body-secondary">
+                  Ravenna, Emilia Romagna, Italia
+                </p>
+                <p className='card-text'>ele.description   </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        
+
+        
+
+       
+        
+        
+
+
+      </div>
+    </div>
   )
 }
