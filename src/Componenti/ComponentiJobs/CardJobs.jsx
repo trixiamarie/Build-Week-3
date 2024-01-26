07c2e3cf3078b60da10cfc7611bcae582fde1451
Gregorio Vecchio <gregoriovecchio@gmail.com>
2024-01-26 16:08:43 +0100

    Aiuto Trixia mi picchia

--- src/Componenti/ComponentiJobs/CardJobs.jsx
+++ src/Componenti/ComponentiJobs/CardJobs.jsx
@@ -3,7 +3,7 @@ import { IoIosHeartEmpty } from "react-icons/io";
 
 export default function CardJobs() {
   return (
-    <div className="lavoroGreg border-bottom mt-3 d-flex justify-content-between mt-5 bg-white">
+    <div className="lavoroGreg mt-3 d-flex justify-content-between bg-white border-bottom">
       <div className="card mb-0 border border-0" style={{ maxWidth: 540 }}>
         <div className="d-flex">
           <div className="flex-shrink-0">
@@ -14,7 +14,7 @@ export default function CardJobs() {
               style={{ width: 60, height: 60 }}
             />
           </div>
-          <div className="flex-grow-1 mx-1 border-bottom">
+          <div className="flex-grow-1 mx-1">
             <div className="card-body ps-2 pt-1">
               <h5 className="card-title mb-1 text-primary">
                 Junior Technical Analyst M&A
@@ -29,8 +29,7 @@ export default function CardJobs() {
               <small className="mb-1 mt-2 text-body-secondary">
                 Promosso ·
                 <span className="fw-medium text-success">
-                  {" "}
-                  {Math.floor(Math.random() * 50) + 1} candidati
+                  {" "+ (Math.floor(Math.random() * 25) + 1)} candidati
                 </span>
               </small>
             </div>
--- src/Pagine/Jobs.jsx
+++ src/Pagine/Jobs.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import CardJobs from '../Componenti/ComponentiJobs/CardJobs'
+import CardJobs from '../Componenti/ComponentiJobs/CardJobs';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import ColonnaDx from '../Componenti/ComponentiJobs/ColonnaDx';
+import ColonnaSx from '../Componenti/ComponentiJobs/ColonnaSx';
 
 export default function Jobs() {
   return (
-    <CardJobs/>
+    <>
+      <Container className='mt-5 bg-white'>
+      <Row>
+        <Col xs={12} md={6}>
+          <ColonnaSx/>
+        </Col>
+        <Col xs={12} md={6}>
+          <div className="p-3">Contenuto Colonna Destra Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque rerum optio voluptate expedita itaque aperiam quis iure atque deserunt numquam magnam ad delectus, voluptates excepturi pariatur earum laudantium beatae accusantium.
+          </div>
+        </Col>
+      </Row>
+    </Container>
+     
+    </>
+   
   )
 }
