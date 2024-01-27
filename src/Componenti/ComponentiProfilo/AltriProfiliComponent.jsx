854a1cd37099f59d28fc448cea3de88e0fbe1fa5
Francesco Cecca <fcecca3@gmail.com>
2024-01-27 19:27:58 +0100

    altriProfiliComponent add

--- src/Componenti/ComponentiProfilo/AltriProfiliComponent.jsx
+++ src/Componenti/ComponentiProfilo/AltriProfiliComponent.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Button, Col, Modal } from "react-bootstrap";
+import { Button, Col, Modal, Card } from "react-bootstrap";
+import {useSelector} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 function ModaleAltriProfiliComponent(props) {
   return (
@@ -31,10 +33,34 @@ function ModaleAltriProfiliComponent(props) {
 
 export default function AltriProfiliComponent() {
   const [modalShow, setModalShow] = React.useState(false);
+  const allProfiles = useSelector(state => state.allProfiles.data)
+  console.log(allProfiles)
+const n = Math.floor(Math.random()*allProfiles.length/2)
+  const profileToShow = allProfiles.slice(n, n+6)
 
   return (
-    <Col md={12} className=" my-3 rounded-1 border border-secondary-subtle">
-      <h6 className="ms-1">Altri profili consultati</h6>
+    <Col md={12} className="rounded-1 border border-secondary-subtle p-3">
+    <h6 className="ms-1 mb-3">Altri profili consultati</h6>
+    {profileToShow.map((profile) => (
+      <Card key={profile.id} className=" shadow-sm">
+        <Card.Body className="d-flex align-items-center">
+          <Link to={`/profile/${profile.id}`} className="text-decoration-none text-dark d-flex align-items-center">
+            <img 
+              src={profile.image}
+              alt="Profilo di ${profile.name}"
+              width={50}
+              height={50}
+              className="border border-3 border-white rounded-circle me-3"
+            />
+            <div className="flex-grow-1">
+              <h5 className="fs-6 mb-1">{`${profile.name} ${profile.surname}`}</h5>
+              <p className="mb-2 text-muted">{profile.title}</p>
+              <button className="btn btn-sm btn-outline-primary rounded-pill">Visualizza</button>
+            </div>
+          </Link>
+        </Card.Body>
+      </Card>
+    ))}
       <Button
         className="w-100 text-center border-top rounded-top-0"
         variant="light"
@@ -50,3 +76,4 @@ export default function AltriProfiliComponent() {
     </Col>
   );
 }
+
