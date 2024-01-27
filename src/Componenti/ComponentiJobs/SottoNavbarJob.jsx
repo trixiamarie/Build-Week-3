c3260f14c4c77c427977699b36204017d24ed5a1
Matteo Nardin <nardin.matteo@gmail.com>
2024-01-27 20:22:22 +0100

    sottoNavJob

---
+++ src/Componenti/ComponentiJobs/SottoNavbarJob.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import "../../Style/SottoNavbarJob.css";
+import { FcLike } from "react-icons/fc";
+import { useSelector } from "react-redux";
+
+
+
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+
+export default function SottoNavbarJob() {
+
+    const userData = useSelector(state => state.user.userData.data)
+
+  //gestione del toggle
+  const [radioValue, setRadioValue] = useState('1');
+  //stato dove memorizzo search|company|category
+  //lascio search come valore iniziale perché i bottone sarà setta su lavoro
+  const [endPoint, setEndPoint] = useState("search");
+
+  const radios = [
+    { name: 'Lavoro', value: '1' , endPoint: "search"},
+    { name: 'Compagnia', value: '2' , endPoint: "company"},
+    { name: 'Categoria', value: '3' , endPoint: "category"},
+  ];
+
+
+  useEffect(()=>{
+    console.log(endPoint)
+  },[endPoint])
+
+
+
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    console.log(innerWidth);
+    window.addEventListener("resize", () => {
+      setInnerWidth(window.innerWidth);
+    });
+    const sottoNavbarUsername = document.querySelector(
+      ".sotto-navbar-btns div:first-of-type"
+    );
+    const sottoNavBarJob = document.querySelector(
+        ".sotto-navbar-job"
+      );
+    innerWidth < 768
+      ? (sottoNavBarJob.style.display = "block")
+      : (sottoNavBarJob.style.display = "block");
+
+    innerWidth < 992
+      ? (sottoNavbarUsername.innerHTML = "···")
+      : (sottoNavbarUsername.innerText = "Aggiungi ai Preferiti");
+
+    return () => {
+      window.removeEventListener("resize", () => {
+        setInnerWidth(window.innerWidth);
+      });
+    };
+  }, [innerWidth]);
+
+
+  return (
+    <div className="sotto-navbar-job">
+      <Container>
+        <div className="sotto-navbar">
+          <div className="sotto-navbar-sx">
+            <ButtonGroup>
+              {radios.map((radio, idx) => (
+                <ToggleButton
+                  key={idx}
+                  id={`radio-${idx}`}
+                  type="radio"
+                  variant={'outline-success'}
+                  name="radio"
+                  value={radio.value}
+                  checked={radioValue === radio.value}
+                  onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  onClick={()=> setEndPoint(radio.endPoint)}
+                >
+                  {radio.name}
+                </ToggleButton>
+              ))}
+            </ButtonGroup>
+          </div>
+          <div className="sotto-navbar-btns">
+            <div>Aggiungi ai Preferiti</div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  )
+}
