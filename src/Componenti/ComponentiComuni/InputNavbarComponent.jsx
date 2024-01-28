22d5ecdba6fe108e23b28f654cd44d65ded59671
Matteo Nardin <nardin.matteo@gmail.com>
2024-01-28 01:52:00 +0100

    inputJob che non salva la query

--- src/Componenti/ComponentiComuni/InputNavbarComponent.jsx
+++ src/Componenti/ComponentiComuni/InputNavbarComponent.jsx
@@ -3,8 +3,25 @@ import { Container, Form, InputGroup } from 'react-bootstrap'
 import { FaLinkedin } from 'react-icons/fa'
 import { IoMdSearch } from 'react-icons/io'
 import { Link } from 'react-router-dom'
+import { getSearchJob } from '../../Action/searchActions'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 export default function InputNavbarComponent() {
+
+  const [query, setQuery] = useState();
+
+  const dispatch = useDispatch()
+
+  const handleQuery = (e) =>{
+    setQuery(e.target.value)
+  }
+  const handleEnter = (e) =>{
+    e.preventDefault()
+    dispatch(getSearchJob(query))
+    console.log("input all'enter", query)
+  }
+
   return (
     <>
         <Container className="d-flex justify-content-flex-start align-items-center ContainerNavbarCustom">
@@ -22,19 +39,22 @@ export default function InputNavbarComponent() {
               alignItems: "center",
             }}
           >
-            <InputGroup className="mx-1">
-              <InputGroup.Text className="border-0 InputCustomNavbar">
-                {" "}
-                <IoMdSearch className="IconSearchCustom" />
-              </InputGroup.Text>
+             <Form onSubmit={handleEnter}>
+              <InputGroup className="mx-1">
+                <InputGroup.Text className="border-0 InputCustomNavbar">
+                  {" "}
+                  <IoMdSearch className="IconSearchCustom" />
+                </InputGroup.Text>
 
-              <Form.Control
-                className="InputCustomNavbar"
-                placeholder="Cerca"
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-              />
-            </InputGroup>
+                <Form.Control
+                  className="InputCustomNavbar"
+                  placeholder="Cerca"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                  onChange = {handleQuery}
+                />
+              </InputGroup>
+            </Form>
           </div>
           <div className="d-block d-md-none" md={4}>
             <input
