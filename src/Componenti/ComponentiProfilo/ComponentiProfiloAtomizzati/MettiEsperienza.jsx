b2d9caf7c6001a3d006412513d7efd36831c4305
VczEno <arpaia.vin@gmail.com>
2024-01-28 16:44:44 +0100

    chiamate EXP

--- src/Componenti/ComponentiProfilo/ComponentiProfiloAtomizzati/MettiEsperienza.jsx
+++ src/Componenti/ComponentiProfilo/ComponentiProfiloAtomizzati/MettiEsperienza.jsx
@@ -3,9 +3,85 @@ import { FaTrash } from "react-icons/fa";
 import { RiPencilLine } from "react-icons/ri";
 import Modal from "react-bootstrap/Modal";
 import { useState } from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
+import { FaPlus } from "react-icons/fa";
+import axios from "axios";
 
 export default function MettiEsperienza({ esperienza }) {
-  const [lgShow, setLgShow] = useState(false);
+  const [id, setId] = useState("65b238dc082963001801ea7e");
+  const [validated, setValidated] = useState(false);
+  const [newExp, setNewExp] = useState({role:'', company:'', startDate: '2022-02-02', endDate: null, description: '', area: ''})
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
+  
+  const addNewExp = (data) => { /* modifica esperienza chiamata PUT */
+    axios
+        .put(
+          `https://striveschool-api.herokuapp.com/api/profile/${id}/experiences/${esperienza._id}`, data,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIyMzhkYzA4Mjk2MzAwMTgwMWVhN2UiLCJpYXQiOjE3MDY0NTMwNjEsImV4cCI6MTcwNzY2MjY2MX0.JMfviXDOUKx0n5GLsvkceHi8BQ2__jm5Uqix4trcPSI",
+            },
+          }
+        )
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+  }
+
+  const deleteExp = () => { /* eliminazione esperienza chiamata DELETE */
+  axios
+      .delete(
+        `https://striveschool-api.herokuapp.com/api/profile/${id}/experiences/${esperienza._id}`,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIyMzhkYzA4Mjk2MzAwMTgwMWVhN2UiLCJpYXQiOjE3MDY0NTMwNjEsImV4cCI6MTcwNzY2MjY2MX0.JMfviXDOUKx0n5GLsvkceHi8BQ2__jm5Uqix4trcPSI",
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      handleClose()
+}
+  
+  
+    const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      console.log(newExp)
+      setValidated(true)
+      addNewExp(newExp)
+  
+    };
+
+  const mesi = ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre']
+
+  const anni = () => {
+    const arr = []
+    for (let y = 2024; y > 1924; y--) {
+      arr.push(y)
+    }
+    return arr
+  }
+
+
+  const [showPutMod, setShowPutMod] = useState(false);
   
   const icone = useRef();
 
@@ -67,28 +143,162 @@ export default function MettiEsperienza({ esperienza }) {
         </div>
 
         <div ref={icone} className="fs-5 me-2 d-flex text-secondary d-none">
-          <div className="matita-btn" onClick={() => setLgShow(true)}>
+          <div className="matita-btn" onClick={() => {setShowPutMod(true)
+                                                      console.log(esperienza)}}>
             <RiPencilLine />
           </div>
-          <div className="matita-btn">
+          <div className="matita-btn" onClick={handleShow}>
             <FaTrash className="gregTrash" />
           </div>
         </div>
       </div>
 
+      
+
+      {/* modale chiamata PUT - modifica esperienza */}
       <Modal
-      className="d-none"
         size="lg"
-        show={lgShow}
-        onHide={() => setLgShow(false)}
+        show={showPutMod}
+        onHide={() => setShowPutMod(false)}
         aria-labelledby="example-modal-sizes-title-lg"
       >
         <Modal.Header closeButton>
           <Modal.Title id="example-modal-sizes-title-lg">
-            Large Modal
+            Modifica esperienza
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body>...</Modal.Body>
+        <p className='text-secondary mx-3'>* Indica che è obbligatorio</p>
+        <Modal.Body className='mx-3'>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+
+            <Form.Group controlId="validationCustom01" className='fs-6 my-3'>
+              <Form.Label className='text-secondary'>Qualifica*</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Esempio: Full StackDeveloper"
+                onChange={(e) =>setNewExp({...newExp, role:e.target.value})}
+
+              />
+            </Form.Group>
+            {/* tipo di impiego select */}
+            <Form.Group controlId="validationCustom02" className='fs-6 my-3'>
+              <Form.Label className='text-secondary' >Nome azienda*</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Esempio: Microsoft"
+                onChange={(e) =>setNewExp({...newExp, company:e.target.value})}
+
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+              <Form.Label className='text-secondary' >Località</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Esempio: Cava dei Selci, Italia"
+                onChange={(e) =>setNewExp({...newExp, area:e.target.value})}
+              />
+            </Form.Group>
+            {/* tipo di località select */}
+            {/* checkbox Attualmente ricopro questo incarico */}
+            {/* Select data inizio e fine  */}
+            <Row className='align-items-end'>
+              <Col>
+                <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+                  <Form.Label className='text-secondary' >Data di inizio*</Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Mese</option>
+                    {mesi.map((m, index) => <option key={index} value={index + 1} >{m}</option>)}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+                  <Form.Label className='text-secondary' ></Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Anno</option>
+                    {anni().map((y, index) => <option key={index} value={index + 1} >{y}</option>)}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Row className='align-items-flex-end'>
+              <Col>
+                <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+                  <Form.Label className='text-secondary' >Data di fine*</Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Mese</option>
+                    {mesi.map((m, index) => <option key={index} value={index + 1} >{m}</option>)}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+                  <Form.Label className='text-secondary' ></Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Anno</option>
+                    {anni().map((y, index) => <option key={index} value={index + 1} >{y}</option>)}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
+
+
+
+
+            <Form.Group controlId="validationCustom04">
+              <Form.Label className='text-secondary' >Settore</Form.Label>
+              <Form.Control type="text" placeholder="" />
+            </Form.Group>
+
+            <Form.Group controlId="validationCustom04">
+              <Form.Label className='text-secondary' >Descrizione</Form.Label>
+              <Form.Control as="textarea"
+                placeholder=""
+                style={{ height: '100px' }} 
+                onChange={(e) =>setNewExp({...newExp, description:e.target.value})}
+                />
+                
+            </Form.Group>
+
+            <Form.Group controlId="validationCustom04">
+              <Form.Label className='text-secondary' >Sommario</Form.Label>
+              <Form.Control type="text" placeholder="Esempio: Baby pensionato" />
+            </Form.Group>
+
+
+            <h3>Competenze</h3>
+            <Button variant='outline-primary' className='fw-semibold fs-5'><FaPlus /> Aggiungi competenza</Button>
+
+            <h3>Media</h3>
+            <Button variant='outline-primary' className='fw-semibold fs-5'><FaPlus /> Aggiungi media</Button>
+
+
+
+
+            <Button type="button" onClick={(handleSubmit)}>Salva</Button>
+          </Form>
+
+        </Modal.Body>
+      </Modal>
+      {/* modale chiamate DELETE - eliminazione esperienza */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cancella esperienza</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Sei sicuro di voler rimuove definitivamente questa esperienza dal tuo profilo?</Modal.Body>
+        <Modal.Footer >
+          <Button variant="secondary" onClick={handleClose}>
+            No
+          </Button>
+          <Button variant="primary" onClick={deleteExp}>
+            Sì
+          </Button>
+        </Modal.Footer>
       </Modal>
     </>
   );
--- src/Componenti/ComponentiProfilo/EsperienzaComponent.jsx
+++ src/Componenti/ComponentiProfilo/EsperienzaComponent.jsx
@@ -7,11 +7,63 @@ import MettiEsperienza from "./ComponentiProfiloAtomizzati/MettiEsperienza";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Modal from 'react-bootstrap/Modal';
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { FaPlus } from "react-icons/fa";
+import { useDispatch } from "react-redux";
 
 export default function EsperienzaComponent() {
   /* const [ id, setId] = useState(useParams()); */
-  const [id, setId] = useState("6551e7bbc55e7e0018f83bfb");
+  const [id, setId] = useState("65b238dc082963001801ea7e");
   const [esperienze, setEsperienze] = useState([]);
+  const [showPostMod, setshowPostMod] = useState(false);
+  
+  const [validated, setValidated] = useState(false);
+  const [newExp, setNewExp] = useState({role:'', company:'', startDate: '2022-02-02', endDate: null, description: '', area: ''})
+  const dispatch= useDispatch()
+
+
+const addNewExp = (data) => {
+  axios
+      .post(
+        `https://striveschool-api.herokuapp.com/api/profile/${id}/experiences/`, data,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIyMzhkYzA4Mjk2MzAwMTgwMWVhN2UiLCJpYXQiOjE3MDY0NTMwNjEsImV4cCI6MTcwNzY2MjY2MX0.JMfviXDOUKx0n5GLsvkceHi8BQ2__jm5Uqix4trcPSI",
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    console.log(newExp)
+    setValidated(true)
+    addNewExp(newExp)
+
+  };
+
+  const mesi = ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre']
+
+  const anni = () => {
+    const arr = []
+    for (let y = 2024; y > 1924; y--) {
+      arr.push(y)
+    }
+    return arr
+  }
 
   useEffect(() => {
     axios
@@ -19,8 +71,9 @@ export default function EsperienzaComponent() {
         `https://striveschool-api.herokuapp.com/api/profile/${id}/experiences/`,
         {
           headers: {
+            'Content-Type': 'application/json',
             Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4M2JmMmMwNTgzNTAwMTg1MjMwZGMiLCJpYXQiOjE3MDYxNzQ1OTIsImV4cCI6MTcwNzM4NDE5Mn0.CyKBDIwBzHkgovr4Z3UIy0SAkGIt3ZvpUsSwkxUWMOg",
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIyMzhkYzA4Mjk2MzAwMTgwMWVhN2UiLCJpYXQiOjE3MDY0NTMwNjEsImV4cCI6MTcwNzY2MjY2MX0.JMfviXDOUKx0n5GLsvkceHi8BQ2__jm5Uqix4trcPSI",
           },
         }
       )
@@ -28,35 +81,169 @@ export default function EsperienzaComponent() {
         console.log(res);
         setEsperienze(res.data);
       })
-      .catch((err) => console.log(console.error()));
+      .catch(function (error) {
+        console.log(error);
+      });
   }, [id]);
 
   return (
     <>
-      {esperienze.length > 0 && (
+      
         <div className="border rounded px-4 pt-3 pb-0 bg-white my-2">
           <div className="d-flex justify-content-between alig-items-start">
             <h4>Esperienza</h4>
 
             <div className="text-secondary fs-5 d-flex">
-              <div className="matita-btn">
-                <IoAddSharp
-                  className=" "
-                  onClick={() => {
-                    console.log("ciao");
-                  }}
-                />
+              <div className="matita-btn" onClick={() => {
+                setshowPostMod(true)
+                console.log('modale PUT exp')
+              }}>
+                <IoAddSharp />
               </div>
             </div>
           </div>
-
+          {esperienze.length > 0 && (
           <div className="esperienze mt-2 mb-0 pb-0">
             {esperienze.map((ele) => (
               <MettiEsperienza key={ele._id} esperienza={ele} />
             ))}
-          </div>
+          </div> )}
         </div>
-      )}
+     
+
+
+      {/* modale per aggiungere una nuova esperienza chiamata POST */}
+      <Modal
+        size="lg"
+        show={showPostMod}
+        onHide={() => setshowPostMod(false)}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-lg">
+            Aggiungi esperienza
+          </Modal.Title>
+        </Modal.Header>
+        <p className='text-secondary mx-3'>* Indica che è obbligatorio</p>
+        <Modal.Body className='mx-3'>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+
+            <Form.Group controlId="validationCustom01" className='fs-6 my-3'>
+              <Form.Label className='text-secondary'>Qualifica*</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Esempio: Full StackDeveloper"
+                onChange={(e) =>setNewExp({...newExp, role:e.target.value})}
+
+              />
+            </Form.Group>
+            {/* tipo di impiego select */}
+            <Form.Group controlId="validationCustom02" className='fs-6 my-3'>
+              <Form.Label className='text-secondary' >Nome azienda*</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Esempio: Microsoft"
+                onChange={(e) =>setNewExp({...newExp, company:e.target.value})}
+
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+              <Form.Label className='text-secondary' >Località</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Esempio: Cava dei Selci, Italia"
+                onChange={(e) =>setNewExp({...newExp, area:e.target.value})}
+              />
+            </Form.Group>
+            {/* tipo di località select */}
+            {/* checkbox Attualmente ricopro questo incarico */}
+            {/* Select data inizio e fine  */}
+            <Row className='align-items-end'>
+              <Col>
+                <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+                  <Form.Label className='text-secondary' >Data di inizio*</Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Mese</option>
+                    {mesi.map((m, index) => <option key={index} value={index + 1} >{m}</option>)}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+                  <Form.Label className='text-secondary' ></Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Anno</option>
+                    {anni().map((y, index) => <option key={index} value={index + 1} >{y}</option>)}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Row className='align-items-flex-end'>
+              <Col>
+                <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+                  <Form.Label className='text-secondary' >Data di fine*</Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Mese</option>
+                    {mesi.map((m, index) => <option key={index} value={index + 1} >{m}</option>)}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="validationCustom03" className='fs-6 my-3'>
+                  <Form.Label className='text-secondary' ></Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Anno</option>
+                    {anni().map((y, index) => <option key={index} value={index + 1} >{y}</option>)}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
+
+
+
+
+            <Form.Group controlId="validationCustom04">
+              <Form.Label className='text-secondary' >Settore</Form.Label>
+              <Form.Control type="text" placeholder="" />
+            </Form.Group>
+
+            <Form.Group controlId="validationCustom04">
+              <Form.Label className='text-secondary' >Descrizione</Form.Label>
+              <Form.Control as="textarea"
+                placeholder=""
+                style={{ height: '100px' }} 
+                onChange={(e) =>setNewExp({...newExp, description:e.target.value})}
+                />
+                
+            </Form.Group>
+
+            <Form.Group controlId="validationCustom04">
+              <Form.Label className='text-secondary' >Sommario</Form.Label>
+              <Form.Control type="text" placeholder="Esempio: Baby pensionato" />
+            </Form.Group>
+
+
+            <h3>Competenze</h3>
+            <Button variant='outline-primary' className='fw-semibold fs-5'><FaPlus /> Aggiungi competenza</Button>
+
+            <h3>Media</h3>
+            <Button variant='outline-primary' className='fw-semibold fs-5'><FaPlus /> Aggiungi media</Button>
+
+
+
+
+            <Button type="button" onClick={(handleSubmit)}>Salva</Button>
+          </Form>
+
+        </Modal.Body>
+      </Modal>
+
+      
     </>
   );
 }
