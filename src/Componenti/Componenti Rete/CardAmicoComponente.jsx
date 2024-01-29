64cea70d99b2ee35d1dbf3b9e0490a95da1433b5
Matteo Nardin <nardin.matteo@gmail.com>
2024-01-29 12:23:12 +0100

    patch CardAmico

--- src/Componenti/Componenti Rete/CardAmicoComponente.jsx
+++ src/Componenti/Componenti Rete/CardAmicoComponente.jsx
@@ -4,11 +4,12 @@ import { HiOutlineDotsHorizontal } from "react-icons/hi";
 export default function CardAmicoComponente() {
   return (
     <>
-        <div className="d-flex justify-content-between align-items-center lineLight" style={{padding: "8px 8px 8px 24px"}}>
+        <div className="d-flex justify-content-between align-items-center lineLight" style={{padding: "8px 8px 8px 24px", positon: "relative"}}>
            
             <div className="d-flex">
                
                 <img
+                // src={image}
                 src="https://www.market-research-companies.in//images/default.jpg"
                 className="img-fluid "
                 alt="..."
@@ -16,16 +17,18 @@ export default function CardAmicoComponente() {
                 />
                
                 <div className="ms-2">
+                    {/* {name}{surname} */}
                     <span className="fs16">Nome Profilo</span>
                     <br></br>
+                    {/* {title} */}
                     <span className="fs14">Fullstack in formazione presso epicode</span>
                     <br></br>
                     <span className="fs12">Collegati 3 giorni fa</span>
                 </div>
             </div>
-            <div>
+            <div className="rete-btn">
                 <button className="btn-messaggio">Messaggio</button>
-                <button id="threeDots"><HiOutlineDotsHorizontal /></button>
+                <button id="threeDots"><HiOutlineDotsHorizontal style={{width: "24px", height: "24px"}}/></button>
             </div>
             
         </div>
