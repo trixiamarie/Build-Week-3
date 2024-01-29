c1ca614b69b3c56aa9a884f75e01aae860398320
trixiamarie <trixialorenzana@gmail.com>
2024-01-29 14:18:10 +0100

    edit

--- src/Componenti/ComponentiProfilo/AttivitaSinglePostComponent.jsx
+++ src/Componenti/ComponentiProfilo/AttivitaSinglePostComponent.jsx
@@ -1,40 +1,102 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { RiPencilLine } from "react-icons/ri";
+import { FaTrash } from "react-icons/fa";
+import axios from "axios";
+import { BEARER_TOKEN } from "../../Config";
+import { useDispatch } from "react-redux";
+import { getAllPostsData } from "../../Action/postsActions";
+import ModalNewPost from "../ComponentiHome/ModalNewPost";
+import ModalModifyPost from "./ModalModifyPost";
 
-export default function AttivitaSinglePostComponent({post}) {
-
+export default function AttivitaSinglePostComponent({ post }) {
+  const [show, setShow] = useState(false);
+  const dispatch = useDispatch();
   const calculateTimeElapsed = (post) => {
     const createdDate = new Date(post.createdAt);
     const currentDate = new Date();
-  
-    const timeDifference = Math.abs(currentDate - createdDate) / 1000; 
-  
-    if (timeDifference < 60) {
-        return "ora";
-    } else if (timeDifference < 3600) {
-        const minutes = Math.floor(timeDifference / 60);
-        return `${minutes} ${minutes === 1 ? 'minuto' : 'minuti'} fa`;
-    } else if (timeDifference < 86400) {
-        const hours = Math.floor(timeDifference / 3600);
-        return `${hours} ${hours === 1 ? 'ora' : 'ore'} fa`;
-    } else {
-        
-        const day = createdDate.getDate();
-        const month = createdDate.getMonth() + 1; 
-        const year = createdDate.getFullYear();
-        return `${day}/${month}/${year}`;
-    }
-};
 
+    const timeDifference = Math.abs(currentDate - createdDate) / 1000;
+
+    if (timeDifference < 60) {
+      return "ora";
+    } else if (timeDifference < 3600) {
+      const minutes = Math.floor(timeDifference / 60);
+      return `${minutes} ${minutes === 1 ? "minuto" : "minuti"} fa`;
+    } else if (timeDifference < 86400) {
+      const hours = Math.floor(timeDifference / 3600);
+      return `${hours} ${hours === 1 ? "ora" : "ore"} fa`;
+    } else {
+      const day = createdDate.getDate();
+      const month = createdDate.getMonth() + 1;
+      const year = createdDate.getFullYear();
+      return `${day}/${month}/${year}`;
+    }
+  };
+
+  const handleDelete = async (post) => {
+    console.log(post._id)
+    const userConfirmed = window.confirm("Sei sicuro di voler cancellare questo post?");
+
+    if (userConfirmed) {
+    try {
+      const response = await axios.delete(
+        `https://striveschool-api.herokuapp.com/api/posts/${post._id}`,
+        {
+          headers: {
+            "Authorization": "Bearer " + BEARER_TOKEN,
+          },
+        }
+      );
+  
+      console.log("Post cancellato con successo", response.data);
+      dispatch(getAllPostsData())
+    } catch (error) {
+      console.error("Errore durante la cancellazione del post", error);
+    }}
+  };
+  
+
+  console.log(post);
+
+  const icone = useRef();
+
+  const mostraIcone = () => {
+    icone.current.classList.remove("d-none");
+  };
+
+  const nascondiIcone = () => {
+    icone.current.classList.add("d-none");
+  };
 
   return (
-    <div className="comment-body mb-3">
+    <>
+      <div className="d-flex justify-content-between mb-2 border-bottom border-1"  onMouseEnter={mostraIcone}
+        onMouseLeave={nascondiIcone}>
+        <div className="d-flex">
+          <img className="me-2 mb-2" src={post.image ? post.image : `https://picsum.photos/600?random=${Math.round(Math.random() * 30)}`} alt="imageofuserspost" style={{ height: "50px", width: "50px"}}
+          />
+          <div className="d-flex flex-column">
           <p className="autore-commento m-0">
-            <b>{post.user.name + ' ' + post.user.surname} </b>
+            <b>{post.user.name + " " + post.user.surname} </b>
             <span>
-              ha commentato un post <b>·</b> {calculateTimeElapsed(post)}
+              ha pubblicato un post <b>·</b> {calculateTimeElapsed(post)}
             </span>
           </p>
           <p className="testo-commento m-0">{post.text}</p>
         </div>
-  )
+        </div>
+        <div ref={icone} className="d-flex d-none" >
+        
+          <div className="matita-btn">
+            <RiPencilLine onClick={() => setShow(true)}/>
+          </div>
+          
+          <div className="matita-btn">
+            <FaTrash onClick={() => handleDelete(post)}/>
+          </div>
+          {show && <ModalModifyPost post={post} show={show} setShow={setShow} />}
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/Componenti/ComponentiProfilo/ModalModifyPost.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { BEARER_TOKEN } from '../../Config';
+import axios from 'axios';
+import { getAllPostsData } from '../../Action/postsActions';
+
+export default function ModalModifyPost(props) {
+  const [postText, setPostText] = useState(props.post.text);
+  const [imageFile, setImageFile] = useState(null);
+  const dispatch = useDispatch();
+  const [show, setShow] = useState(true);
+
+  const handleTextChange = (e) => setPostText(e.target.value);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setImageFile(file);
+  };
+
+  const handleClose = () => setShow(false);
+
+  const handlePost = async (e) => {
+    e.preventDefault()
+    console.log("Posting:", postText);
+    console.log(props.post._id)
+  
+    const formData = new FormData();
+    formData.append("text", postText);
+    console.log(formData)
+
+    if (imageFile) {
+        formData.append("image", imageFile);
+      }
+  
+    try {
+      const response = await axios.put(
+        `https://striveschool-api.herokuapp.com/api/posts/${props.post._id}`,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': 'Bearer ' + BEARER_TOKEN,
+          },
+          body: formData
+        }
+      );
+  
+      console.log('Success:', response.data);
+  
+      dispatch(getAllPostsData());
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  
+    setShow(false);
+  };
+  
+
+  return (
+    <Modal show={show} onHide={handleClose} className=''>
+      <Modal.Header closeButton>
+        <div className="d-flex align-items-center">
+        <div className="d-flex align-items-center">
+                    <img
+                        width={48}
+                        height={48}
+                        className="rounded-circle me-2 object-fit-cover"
+                        alt="user"
+                        src={props.post.user.image}
+                    />
+                    <div>
+                        <p className="fw-bold mb-0">
+                            {props.post.user.name + " " + props.post.user.surname}
+                        </p>
+                    </div>
+                </div>
+        </div>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              className="border-1"
+              placeholder="Di cosa vorresti parlare?"
+              as="textarea"
+              value={postText}
+              onChange={handleTextChange}
+              rows={6}
+            />
+            <Form.Label className='mt-3'>Allega:</Form.Label>
+            <Form.Control
+              className=""
+              type="file"
+              onChange={handleFileChange}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Chiudi
+        </Button>
+        <Button variant="primary" onClick={handlePost}>
+          Pubblica
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
